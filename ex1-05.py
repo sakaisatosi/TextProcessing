0d463ef1fbdcd0a1790a4a7ba07736f335f8cffb
Satosi Sakai <s12369ss@gmail.com>
2015-07-04 01:22:55 +0900

    ex5 fixed

--- ex1-05.py
+++ ex1-05.py
@@ -1,9 +1,20 @@
 # coding: utf-8
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
 
-def main(word):
-	# [n:m:s]はs個飛ばしでn番目からm-1番目の要素を参照
-	print "".join(list(word)[::2]).encode("utf-8")
+def ngram(n, style, text):
+	if style == "char":
+		for i in range(0, len(list(text))+1-n):
+			result = ""
+			for j in range(0, n):
+				result += list(text)[i+j]
+			print result
+	elif style == "word":
+		word_list = text.split(" ")
+		for i in range(0, len(word_list)+1-n):
+			result = ""
+			for j in range(0, n):
+				result += word_list[i+j] + " "
+			print result
 
-if __name__ == '__main__': # javaのmain関数のようなもの
-	main(u"パタトクカシーー")
+if __name__ == '__main__': 
+	ngram(3, "word", "I am an NLPer")
